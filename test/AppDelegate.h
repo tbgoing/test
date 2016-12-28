652e07919223222545aec40c9f6e610c9623cfc0
tb <td06@21688.com>
2016-12-28 11:31:59 +0800

    hello

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by tb on 16/12/28.
+//  Copyright © 2016年 annto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
